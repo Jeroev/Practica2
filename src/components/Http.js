f149b725a43bb5333dd79fb61f3f1a9310572b5c
Jerónimo Echeverri Vásquez <68303554+jeroev@users.noreply.github.com>
2021-05-02 21:17:06 -0500

    Archivos agregados

--- src/components/Http.js
+++ src/components/Http.js
@@ -1,13 +1,24 @@
 import {createElement, addChild} from '../utils/createdElements.js'
 
+
+
+export function searchPhrase(InsideLink, paragraphClass){
+    fetch('https://idwapi-juvasquez88.vercel.app/js')
+        .then(response =>response.json())
+        .then(receive => Object.values(receive['phrases']))
+        .then(datos => {addData(datos, InsideLink, paragraphClass)})
+}
+
 //recorre el objeto que entrega la busqueda en el fetch,
 // phrase: palabra
 //entonces se abre dos veces la lista
-export function addData(data, InsideLink){
+export function addData(data, InsideLink, paragraphClass){
+    let paragraph =  document.getElementById(paragraphClass)
+    var pointer = 0
     for (const key in data){
         let match = InsideLink.value  //lo que esta en la barra de busqueda
         let listElement = String(Object.values(data[key])) //Segundo objeto
-        let child = document.createElement('p')
+        let child = document.createElement('p', {}, [])
         let flag = true
         for (let i = 0; i < match.length; i++) {
             if (listElement[i].toUpperCase() != match[i].toUpperCase()) {
@@ -18,15 +29,16 @@ export function addData(data, InsideLink){
         if (flag == false) {continue} //sigue con la otra iteracion
         child.appendChild(document.createTextNode(listElement))
         description.appendChild(child)
+        pointer += 1
+    }
+    if (pointer == 0 && InsideLink.value.length > 0) {
+        let child = createElement('p', {id: 'childNot'},[])
+        child.appendChild(document.createTextNode('Word not found'))
+        description.appendChild(child)
     }
 }
 
-export function searchPhrase(InsideLink){
-    fetch('https://idwapi-juvasquez88.vercel.app/js')
-        .then(response =>response.json())
-        .then(receive => Object.values(receive['phrases']))
-        .then(datos => {addData(datos, InsideLink)})
-}
+
 
 export function deletePhrase() {
     description.innerHTML = ''
